8740f1842776824c1501e420123bd8337db110f2
Dimitri Barbe <dbarbe2013@gmail.com>
2023-02-08 04:52:29 -0500

    async

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ const csv =require('csvtojson')
 const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-//initialize dynamodb put object
-var dynamodbparams = {};
-var failedinserts = [];
-var successfulinserts = 0;
 
 //S3 getobject function
 async function S3GetObject(bucket,key) {
@@ -22,41 +18,56 @@ async function S3GetObject(bucket,key) {
 }
 
 //DynamoDB Put Item function
-async function dynamoDBPut(csvitem) {
+async function dynamoDBPut(csvitem,tablename) {
+  var dynamodbparams = {
+    TableName: tablename
+  };
   //initialize DynamoDB row parameters with CSV row data, tablename initalized in function handler
   dynamodbparams.Item = csvitem;
   //console.log(dynamodbparams);
   try{
     await dynamodb.put(dynamodbparams).promise();
-    console.log("SUCCESS: ",dynamodbparams.Item);
-    successfulinserts++;
+    return csvitem;
   }catch(error){
-    console.log("FAILED: ",dynamodbparams.Item);
-    failedinserts[failedinserts.length] = dynamodbparams.Item; //Insert failed insert requets to an array to report at end of function
+    console.log("FAIL: ", csvitem);
+    throw new Error();
   }
 }
 
 
 exports.handler = async function(event, context) {
+  
+    let successfulinserts = 0;
+    let failedinserts = 0;
     
-    dynamodbparams.TableName = event.tablename;
     //Find file in S3 based on event parameters
     let csvbody = await S3GetObject(event.bucket,event.key);
     //console.log(csvbody);
 
-
+    let rowpromises = [];
     //Convert CSV File into Json Object
-    let jsonfilecontent = await csv().fromString(csvbody).subscribe();
-    //console.log("JSON File Content: ",jsonfilecontent[0]);
-
-    for (let i = 0; i < jsonfilecontent.length; i++) {
-      await dynamoDBPut(jsonfilecontent[i]);
-    }
+    await csv().fromString(csvbody).subscribe((json)=>{
+       rowpromises.push(dynamoDBPut(json,event.tablename));
+    });
     
-    console.log("FAILED INSERTS: ",failedinserts); //log 
-    const response = {
+    Promise.allSettled(rowpromises)
+      .then((result) => {
+        result.forEach(element => {
+          if(element.status == 'fulfilled'){
+            console.log("SUCCESS");
+            successfulinserts++;
+          }else{
+            failedinserts++;
+          }
+        });
+        console.log(successfulinserts," Successful Inserts");
+        console.log(failedinserts," Failed Inserts");
+      });
+        
+        
+    let response = {
         statusCode: 200,
-        body: successfulinserts +" Successful Inserts. " + (failedinserts.length) + " Failed Inserts.",
+        body: "CSV Uploaded.",
     };
     return response;
 };
